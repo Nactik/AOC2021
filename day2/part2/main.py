bc326f3009e1c73d3e2babe92eaf9496d1435b40
Nathan Capiaux <nathan.capiaux@ig2i.centralelille.fr>
2021-12-02 19:04:28 +0100

    day 2 part 2

---
+++ day2/part2/main.py
@@ -0,0 +1,29 @@
+def intrepret_command( command :str, val:int):
+    global horizontal;
+    global depth;
+    global aim;
+
+    if command == "forward" :
+        horizontal += val;
+        depth += (aim * val);
+    elif command == "down" :
+        aim += val;
+    elif command == "up" :
+        aim -= val;
+
+
+f = open("data.txt","r");
+depth = 0;
+horizontal = 0;
+aim = 0;
+
+for line in f : 
+
+    (command, value) = tuple(line.split(' '));
+    intrepret_command(command,int(value));
+
+
+print(f"Output : {depth * horizontal}");
+
+f.close();
+
