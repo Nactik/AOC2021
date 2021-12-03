33ffdbcb384a07d7209c3a5dcd151c72c88530a3
Nathan Capiaux <nathan.capiaux@ig2i.centralelille.fr>
2021-12-03 23:03:11 +0100

    Day3 part1 done

--- day3/part1/main.py
+++ day3/part1/main.py
@@ -1,12 +1,32 @@
 from functools import reduce
 import numpy as np
 
+def mostCommon(element):
+    return 1 if np.count_nonzero(element) > len(element)/2 else 0;
 
-f = open("test.txt","r");
-
-values = [[int(i) for i in line.strip()] for line in f]
-
+def leastCommon(element):
+    return 0 if np.count_nonzero(element) > len(element)/2 else 1;
 
 
+f = open("data.txt","r");
 
-f.close()
+gamma=0;
+epsilon=0;
+
+values = np.array([[int(i) for i in line.strip()] for line in f]);
+
+#On transpose la matrice afin d'avoir le contenu de chaque colonne par ligne
+mostCommonBits = list(map(mostCommon, np.transpose(values)));
+leastCommonBits = list(map(leastCommon, np.transpose(values)));
+
+#On transforme le résultat en bin puis en décimal
+for i in range(0,len(mostCommonBits)):
+    power = len(mostCommonBits) - 1 -i;
+    gamma += (mostCommonBits[i] * (2**power));
+    epsilon += (leastCommonBits[i] * (2**power));
+
+
+
+print(f"Output : {gamma * epsilon}");
+
+f.close();
