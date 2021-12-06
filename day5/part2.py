cba2f56b08b56749b71f28e22e62791778c75dd6
Nathan Capiaux <nathan.capiaux@ig2i.centralelille.fr>
2021-12-06 01:44:57 +0100

    day5 part2

---
+++ day5/part2.py
@@ -0,0 +1,41 @@
+import numpy as np
+import re 
+
+f = open("input/data.txt",'r');
+
+diagram = np.zeros((1000,1000),dtype=int);
+
+# On rempli le tableau
+for line in f:
+    (col1,row1,col2,row2) = tuple([int(i) for i in re.split(" -> |,",line.strip())]);
+    
+    if(row1 == row2) :
+        #On inverse si x1 > x2
+        if (col1 > col2):
+            diagram[row1,col2:col1+1] += 1; 
+        else : 
+            diagram[row1,col1:col2+1] += 1;
+    elif(col1 == col2) :
+        if(row1 > row2) :
+            diagram[row2:row1+1,col1] += 1;
+        else:
+            diagram[row1:row2+1,col1] += 1;
+
+    # diagonales
+    else :
+        stepRow = 1;
+        stepCol = 1;
+        if(col1 > col2) :
+            stepCol = -1;
+        if(row1 > row2) :
+            stepRow = -1;
+
+        for i in range(0, abs(row2 - row1) +1):
+            rowIdx = row1 + (i*stepRow);
+            colIdx = col1 + (i*stepCol);
+            diagram[rowIdx,colIdx] +=1
+
+print(diagram)
+print(f"Ouput : {np.count_nonzero(diagram >= 2)}")
+
+f.close();
