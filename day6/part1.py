4c61763bc37ab76d99cee3edabc158bcac52f301
Nathan Capiaux <nathan.capiaux@ig2i.centralelille.fr>
2021-12-07 00:20:04 +0100

    day6 part1 and 2

--- day6/part1.py
+++ day6/part1.py
@@ -1,17 +1,20 @@
 import numpy as np
-f = open("input/test.txt","r");
+f = open("input/data.txt","r");
 
-fishTab = [int(i) for i in f.readline().split(',')]
-nbDays = 80;
-
+fishTab = np.array([int(i) for i in f.readline().split(',')])
+nbDays = 256;
 
 
 for i in range(0,nbDays):
     nbZero = np.count_nonzero(fishTab == 0)
     if(nbZero > 0):
         for j in range(0,nbZero):
-            fishTab.append(9);
+            fishTab = np.append(fishTab,9);
+    fishTab -= 1;
+    fishTab = np.where(fishTab < 0,6, fishTab)
+    
 
+print(f"Output : {len(fishTab)}")
         
 
     
